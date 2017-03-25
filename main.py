3468e81896c77a3dad6cf41140a401c81d9d0576
David <david.mora6162@gmail.com>
2017-03-25 09:08:55 -0600

    Adding main function

---
+++ main.py
@@ -0,0 +1,11 @@
+def main():
+    counter = 0
+
+    while(counter <= 10):
+        input("Ingrese el nombre del jugador")
+        input("Ingrese la posicion del jugador")
+
+        counter = counter + 1
+
+if __name__ == '__main__':
+    main()
